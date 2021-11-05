c268596d4fd005e064ba238d705b31b6aadc77b9
Kristian Kinchev <83530894+kinchev@users.noreply.github.com>
2021-11-05 18:44:30 +0200

    Solved task from AlgoExpert.

--- DataStructuresAndAlgorithm/AlgoExpert/src/SortedSquaredArray.java
+++ DataStructuresAndAlgorithm/AlgoExpert/src/SortedSquaredArray.java
@@ -2,14 +2,24 @@ import java.util.Arrays;
 import java.util.Scanner;
 
 public class SortedSquaredArray {
-    Scanner scanner = new Scanner(System.in);
-    int[] inputArray = Arrays.stream(scanner.nextLine().split(", ")).mapToInt(Integer::parseInt).toArray()
+    public static void main(String[] args) {
 
-    public int[] sortedSquaredArray(int[] array) {
-        int[] squaredNumbers = new int[array.length];
+        Scanner scanner = new Scanner(System.in);
+        int[] inputArray = Arrays.stream(scanner.nextLine().split(", "))
+                .mapToInt(Integer::parseInt)
+                .toArray();
 
-
+        System.out.println(sortedSquaredArray(inputArray));
     }
 
+    public static int[] sortedSquaredArray(int[] array) {
+        int[] newArray = new int[array.length];
+        for (int i = 0; i < array.length; i++) {
+            newArray[i] = array[i] * array[i];
 
+
+        }
+        Arrays.sort(newArray);
+        return newArray;
+    }
 }
