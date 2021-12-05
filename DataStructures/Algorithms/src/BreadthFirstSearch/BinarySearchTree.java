a07b2747aa6121a5e960ae5472c4e39529660c5e
Kristian Kinchev <83530894+kinchev@users.noreply.github.com>
2021-12-05 14:02:35 +0200

    Rearrange

---
+++ DataStructures/Algorithms/src/BreadthFirstSearch/BinarySearchTree.java
@@ -0,0 +1,56 @@
+package BreadthFirstSearch;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+class BinarySearchTree<E> {
+    private static final String VALUE_CANNOT_BE_NULL = "Value cannot be null." ;
+    private E value;
+    private BinarySearchTree<E> left;
+    private BinarySearchTree<E> right;
+
+    public BinarySearchTree(E value) {
+        if (value == null) {
+            throw new IllegalArgumentException(VALUE_CANNOT_BE_NULL);
+        }
+        this.value = value;
+        left = null;
+        right = null;
+    }
+
+    public E getRootValue() {
+        return value;
+    }
+
+
+    public BinarySearchTree<E> getLeftTree() {
+        return left;
+    }
+
+
+    public BinarySearchTree<E> getRightTree() {
+        return right;
+    }
+
+    public List<E> bfs() {
+        List<E> result = new ArrayList<>();
+        Queue<BinarySearchTree<E>> queue = new LinkedList<>();
+        queue.add(this);
+        while (!queue.isEmpty()) {
+            BinarySearchTree<E> nextNode = queue.poll();
+            result.add(nextNode.getRootValue());
+            if (nextNode.left != null) {
+                queue.add(nextNode.left);
+            }
+            if (nextNode.right != null) {
+                queue.add(nextNode.right);
+            }
+        }
+        return  result;
+    }
+
+
+
+}
--- DataStructures/Big O/ConstantComplexity.java
+++ DataStructures/Big O/ConstantComplexity.java
@@ -2,7 +2,7 @@ package com.telerikacademy.dsa;
 
 public class ConstantComplexity {
     public static void main(String[] args) {
-
+        System.out.println(sum(1,2));
     }
 
     static int sum(int a, int b) {
