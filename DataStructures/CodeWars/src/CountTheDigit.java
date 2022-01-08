b1e4c2d9dd87773f227142b4f995a54fd8abeab1
Kristian Kinchev <83530894+kinchev@users.noreply.github.com>
2022-01-08 14:55:18 +0200

    CodeWars

---
+++ DataStructures/CodeWars/src/CountTheDigit.java
@@ -0,0 +1,24 @@
+import org.junit.platform.commons.util.StringUtils;
+
+public class CountTheDigit {
+    public static int nbDig(int n, int d) {
+        int count = 0;
+        for (int i = 0; i <= n; i++) {
+            int sqrt = i * i;
+            String a = String.valueOf(sqrt);
+            if (a.contains(String.valueOf(d))) {
+                String spaces = a.replaceAll(String.valueOf(d), " ");
+                long q = spaces.chars().filter(c -> c == (int) ' ').count();
+                count+=q;
+            }
+
+
+        }
+        return count;
+
+    }
+
+    public static void main(String[] args) {
+        System.out.println(nbDig(25, 1));
+    }
+}
