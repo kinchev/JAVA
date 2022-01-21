8de0235a5a240d3264c0d60b72f7dad217ce370b
Kristian Kinchev <83530894+kinchev@users.noreply.github.com>
2022-01-21 23:19:17 +0200

    LeetCode

---
+++ DataStructures/Leetcode/EASY/src/PalindromeNumber.java
@@ -0,0 +1,16 @@
+public class PalindromeNumber {
+    public static  boolean isPalindrome(int x) {
+        String xToStr=String.valueOf(x);
+       StringBuilder check=new StringBuilder(xToStr);
+       if(xToStr.equals(check.reverse().toString())){
+           return true;
+       }else{
+           return false;
+       }
+    }
+
+
+    public static void main(String[] args) {
+        System.out.println(isPalindrome(1000021));
+    }
+}
