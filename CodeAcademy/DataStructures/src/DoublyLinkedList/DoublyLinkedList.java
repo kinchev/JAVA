84a760c9d1078d5fdb4512dd8cf671e46b0f0658
Kristian Kinchev <83530894+kinchev@users.noreply.github.com>
2022-02-12 16:50:06 +0200

    CodeAcademy

---
+++ CodeAcademy/DataStructures/src/DoublyLinkedList/DoublyLinkedList.java
@@ -0,0 +1,100 @@
+package DoublyLinkedList;
+
+public class DoublyLinkedList {
+
+    public Node head;
+    public Node tail;
+
+    public DoublyLinkedList() {
+        this.head = null;
+        this.tail = null;
+    }
+
+    public void addToHead(String data) {
+        Node newHead = new Node(data);
+        Node currentHead = this.head;
+
+        if (currentHead != null) {
+            currentHead.setPreviousNode(newHead);
+            newHead.setNextNode(currentHead);
+        }
+        this.head = newHead;
+
+        if (this.tail == null) {
+            this.tail = newHead;
+        }
+    }
+    public void addToTail(String data){
+        Node newTail = new Node(data);
+        Node currentTail=this.tail;
+        if(currentTail != null) {
+            currentTail.setNextNode(newTail);
+            newTail.setPreviousNode(currentTail);
+        }
+        this.tail=newTail;
+        if(head == null) {}
+            this.head=newTail;
+
+    }
+
+    public String removeHead(){
+        Node removedHead=this.head;
+        if(removedHead == null) {
+            return null;
+        }
+        this.head=removedHead.getNextNode();
+        if(this.head != null){
+            this.head.setPreviousNode(null);
+        }
+        if(removedHead==this.tail){
+            // this.removeTail();
+
+        }
+        return removedHead.data;
+    }
+    public String printList() {
+        Node currentNode = this.head;
+        String output = "<head> ";
+        while (currentNode != null) {
+            output += currentNode.data + " ";
+            currentNode = currentNode.getNextNode();
+        }
+        output += "<tail>";
+        System.out.println(output);
+        return output;
+    }
+    public String removeTail(){
+        Node removedTail=this.tail;
+        if(removedTail==null){
+            return  null;
+        }
+        this.tail=removedTail.getPreviousNode();
+        if(this.tail!=null){
+            this.tail.setNextNode(null);
+        }
+        if(removedTail==this.head){
+            this.removeHead();
+        }
+        return removedTail.data;
+    }
+
+    public Node removeByData(String data){
+        Node nodeToRemove=null;
+        Node currentNode=this.head;
+        while(currentNode!=null){
+            if(currentNode.data==currentNode.getNextNode().data){
+                nodeToRemove=currentNode;
+                break;
+            }
+            currentNode=currentNode.getNextNode();
+        }
+        if(nodeToRemove==null){
+            return null;
+        }
+        return nodeToRemove;
+    }
+    public static void main(String[] args) {
+
+    }
+
+}
---
+++ DataStructures/AlgoExpert/src/RemoveDuplicatesFromLinkedList.java
@@ -0,0 +1,5 @@
+public class RemoveDuplicatesFromLinkedList {
+    public static void main(String[] args) {
+
+    }
+}
