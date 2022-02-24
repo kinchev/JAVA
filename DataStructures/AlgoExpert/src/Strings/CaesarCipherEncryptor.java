28ff29db0b97944f3fcaf1868f3b165ee073f18f
Kristian Kinchev <83530894+kinchev@users.noreply.github.com>
2022-02-24 14:57:16 +0200

    AlgoExpert

--- DataStructures/AlgoExpert/src/Strings/CaesarCipherEncryptor.java
+++ DataStructures/AlgoExpert/src/Strings/CaesarCipherEncryptor.java
@@ -1,16 +1,24 @@
 package Strings;
 
 public class CaesarCipherEncryptor {
+    public static String caesarCypherEncryptor(String str, int key) {
+        StringBuilder result = new StringBuilder();
+        for (char c : str.toCharArray()) {
+            int ascii = (c + (key%26));
+            if (ascii <= 122) {
+                result.append((char)ascii);
+            } else {
+                ascii = 96 + ascii % 122;
+                result.append((char)ascii);
+            }
+        }
+
+
+        return result.toString();
+    }
+
     public static void main(String[] args) {
         System.out.println(caesarCypherEncryptor("xyz", 2));
     }
 
-    public static String caesarCypherEncryptor(String str, int key) {
-        StringBuilder result = new StringBuilder();
-        for(char c : str.toCharArray()){
-            int ascii = c+2;
-            result.append((char)ascii);
-        }
-        return result.toString();
-    }
 }
--- DataStructures/AlgoExpert/src/Strings/ReverseString.java
+++ DataStructures/AlgoExpert/src/Strings/ReverseString.java
@@ -10,6 +10,6 @@ public class ReverseString {
         }
 
     public static void main(String[] args) {
-
+        System.out.println(isPalindrome("dasdasdasd"));
     }
 }
