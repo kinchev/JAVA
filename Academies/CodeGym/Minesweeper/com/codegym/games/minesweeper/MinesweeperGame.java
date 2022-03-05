362e61911a0f61bb5668684c8de07e7849d60edd
Kristian Kinchev <83530894+kinchev@users.noreply.github.com>
2022-03-05 10:11:20 +0200

    CodeGym

---
+++ Academies/CodeGym/Minesweeper/com/codegym/games/minesweeper/MinesweeperGame.java
@@ -0,0 +1,14 @@
+public class MinesweeperGame {
+    private static final int SIDE = 9;
+    private final GameObject[][] gameField = new GameObject[SIDE][SIDE];
+
+    private void createGame() {
+        for (int i = 0; i < SIDE; i++) {
+
+        }
+    }
+
+    public void initialize() {
+        setScreenSize(SIDE, SIDE);
+    }
+}
