881a45bccbfc40d63b921b9c276e37da9f15ebb0
Kristian Kinchev <83530894+kinchev@users.noreply.github.com>
2022-03-15 11:21:46 +0200

    Coffee Machine

--- Academies/JetBrains/Coffee Machine/Coffee Machine/task/src/machine/CoffeeMachine.java
+++ Academies/JetBrains/Coffee Machine/Coffee Machine/task/src/machine/CoffeeMachine.java
@@ -1,14 +1,142 @@
 package machine;
 
+import java.util.Scanner;
+
 public class CoffeeMachine {
+
+
+    public static void action(String input) {
+        Scanner scanner = new Scanner(System.in);
+        int water = 400;
+        int milk = 540;
+        int coffeeBeans = 120;
+        int cups = 9;
+        int totalMoney = 550;
+
+        while (!input.equals("exit")) {
+
+            switch (input) {
+                case "take":
+                    System.out.printf("I gave you $%d", totalMoney);
+                    totalMoney -= totalMoney;
+
+                    break;
+                case "fill":
+                    System.out.println("Write how many ml of water you want to add:");
+
+                    water += Integer.parseInt(scanner.nextLine());
+                    System.out.println("Write how many ml of milk you want to add:");
+                    milk += Integer.parseInt(scanner.nextLine());
+                    System.out.println("Write how many grams of coffee beans you want to add:");
+                    coffeeBeans += Integer.parseInt(scanner.nextLine());
+                    System.out.println("Write how many disposable cups of coffee you want to add:");
+                    cups += Integer.parseInt(scanner.nextLine());
+
+                    break;
+                case "remaining":
+                    System.out.println("The coffee machine has:");
+                    System.out.printf("%d ml of water\n", water);
+                    System.out.printf("%d ml of milk\n", milk);
+                    System.out.printf("%d g of coffee beans\n", coffeeBeans);
+                    System.out.printf("%d disposable cups\n", cups);
+                    System.out.printf("$%d of money", totalMoney);
+                    break;
+
+
+                case "buy":
+                    System.out.println("What do you want to buy? 1 - espresso, 2 - latte, 3 - cappuccino:");
+                    String command = scanner.nextLine();
+                    switch (command) {
+                        case "3":
+                            if (water - 200 >= 0 && milk - 100 >= 0 && coffeeBeans - 12 >= 0 && cups - 1 >= 0) {
+                                System.out.println("I have enough resources, making you a coffee!");
+                                water -= 200;
+                                milk -= 100;
+                                coffeeBeans -= 12;
+                                cups -= 1;
+                                totalMoney += 6;
+
+                                break;
+                            } else {
+                                if (water - 200 < 0) {
+                                    System.out.println("Sorry, not enough water!");
+                                } else if (milk - 100 < 0) {
+                                    System.out.println("Sorry, not enough milk!");
+                                } else if (coffeeBeans - 12 < 0) {
+                                    System.out.println("Sorry, not enough coffee beans!");
+                                } else {
+                                    System.out.println("Sorry, not enough cups!");
+                                }
+                            }
+                            break;
+                        case "2":
+                            if (water - 350 >= 0 && milk - 75 >= 0 && coffeeBeans - 20 >= 0 && cups - 1 >= 0) {
+                                System.out.println("I have enough resources, making you a coffee!");
+                                water -= 350;
+                                milk -= 75;
+                                coffeeBeans -= 20;
+                                cups -= 1;
+                                totalMoney += 7;
+
+
+                                break;
+                            } else {
+                                if (water - 350 < 0) {
+                                    System.out.println("Sorry, not enough water!");
+                                } else if (milk - 75 < 0) {
+                                    System.out.println("Sorry, not enough milk!");
+                                } else if (coffeeBeans - 20 < 0) {
+                                    System.out.println("Sorry, not enough coffee beans!");
+                                } else {
+                                    System.out.println("Sorry, not enough cups!");
+                                }
+                            }
+                            break;
+                        case "1":
+                            if (water - 250 >= 0 && coffeeBeans - 16 >= 0 && cups - 1 >= 0) {
+
+                                System.out.println("I have enough resources, making you a coffee!");
+                                water -= 250;
+                                coffeeBeans -= 16;
+                                cups -= 1;
+                                totalMoney += 4;
+//                                System.out.println("The coffee machine has:");
+//                                System.out.printf("%d ml of water\n", water -= 250);
+//                                System.out.printf("%d ml of milk\n", milk);
+//                                System.out.printf("%d g of coffee beans\n", coffeeBeans -= 16);
+//                                System.out.printf("%d disposable cups\n", cups -= 1);
+//                                System.out.printf("%d$ of money\n", totalMoney += 4);
+                                break;
+
+                            } else {
+                                if (water - 250 < 0) {
+                                    System.out.println("Sorry, not enough water!");
+
+                                } else if (coffeeBeans - 16 < 0) {
+                                    System.out.println("Sorry, not enough coffee beans!");
+                                } else {
+                                    System.out.println("Sorry, not enough cups!");
+                                }
+                                break;
+                            }
+
+
+                    }
+            }
+            System.out.println("Write action (buy, fill, take, remaining, exit):");
+            input = scanner.nextLine();
+        }
+    }
+
     public static void main(String[] args) {
-        System.out.println("Starting to make a coffee");
-        System.out.println("Grinding coffee beans");
-        System.out.println("Boiling water");
-        System.out.println("Mixing boiled water with crushed coffee beans");
-        System.out.println("Pouring coffee into the cup");
-        System.out.println("Pouring some milk into the cup");
-        System.out.println("Coffee is ready!");
+
+
+        Scanner scanner = new Scanner(System.in);
+
+
+        System.out.println("Write action (buy, fill, take, remaining, exit):");
+        String input = scanner.nextLine();
+        action(input);
 
     }
 }
---
+++ DesingPatterns/a/src/tele.java
@@ -0,0 +1,12 @@
+//Write a method in any programming language you like that is checking whether a given array is "telescopic".
+//An array is called telescopic if its outer elements are 1's, then the next inner elements are 2's and so on. There is exactly one telescopic array of a given length. Here are the first five telescopic arrays with size 1, 2, 3, 4, 5:
+
+public class tele {
+
+
+
+
+    public static void main(String[] args) {
+
+    }
+}
