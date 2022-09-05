3feb86f86878517c23e7a750cd3dd8a0ffa92267
kinchev <kinchev@yahoo.com>
2022-09-05 19:33:47 +0300

    Snake Game

--- Academies/FreeCodeCamp/app/src/test/java/freecodecamp/PersonTest.java
+++ Academies/FreeCodeCamp/app/src/test/java/freecodecamp/PersonTest.java
@@ -1,6 +1,9 @@
 package freecodecamp;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assumptions.assumeTrue;
+
 import org.junit.jupiter.api.Test;
 
 import lesson2.Person;
@@ -25,5 +28,19 @@ public class PersonTest {
         Person sintiq=new Person();
         assertEquals(3,Person.count );
         
+    }
+    @Test
+
+    public void demostrateBoolean(){
+        int i=4;
+        int j=3;
+
+        if(i>j){
+            System.out.println("Cool");
+        }else{
+            System.out.println("Not cool");
+        }
+        assertEquals(, actual);
+
     }
 }
---
+++ Snake/SnakeGame/src/StartSnake.java
@@ -0,0 +1,5 @@
+public class StartSnake{
+    public static void main(String[] args) {
+        new GameFrame();
+    }
+}
